9a7493dd312826928b7ed7785bee429685a7351f
AirP0WeR <mgyp@mail.ru>
2023-01-29 12:04:09 +0300

    staging test comlete

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -1,5 +1,5 @@
 const { assert, expect } = require("chai");
-const { network, getNamedAccounts, deployments, ethers } = require("hardhat");
+const { network, getNamedAccounts, ethers } = require("hardhat");
 const {
   developmentChains,
   networkConfig,
@@ -8,17 +8,52 @@ const {
 developmentChains.includes(network.name)
   ? describe.skip
   : describe("Raffle Unit Tests", function () {
-      let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer;
+      let raffle, raffleEntranceFee, deployer;
 
       beforeEach(async () => {
         deployer = (await getNamedAccounts()).deployer;
-        await deployments.fixture(["all"]);
         raffle = await ethers.getContract("Raffle", deployer);
-        vrfCoordinatorV2Mock = await ethers.getContract(
-          "VRFCoordinatorV2Mock",
-          deployer
-        );
         raffleEntranceFee = await raffle.getEnteranceFee();
-        interval = await raffle.getInterval();
+      });
+
+      describe("fulfillRandomWords", () => {
+        it("worcks with Chainlink Keepers and Chainlink VRF, we get a ramdom winner", async () => {
+          const startingTimeStamp = await raffle.getLatestTimeStamp();
+          const accounts = await ethers.getSigners();
+
+          await new Promise(async (resolve, reject) => {
+            raffle.once("WinnerPicked", async () => {
+              console.log("WinnerPicked enent fired!");
+
+
+              try {
+                const recentWinner = await raffle.getRecentWinner();
+                const raffleState = await raffle.getRaffleState();
+                const winnerEndingBalance = await accounts[0].getBalance();
+                const endingTimeStamp = await raffle.getLatestTimeStamp();
+
+                await expect(raffle.getPlayer(0)).to.be.reverted;
+                assert.equal(recentWinner.toString(), accounts[0].address);
+                assert.equal(raffleState, 0);
+                assert.equal(
+                  winnerEndingBalance.toString(),
+                  winnerStartingBalance.add(raffleEntranceFee).toString())
+                assert(endingTimeStamp >startingTimeStamp);
+                resolve();
+
+              } catch(error) {
+                console.log(error)
+                reject(error);
+              }
+            });
+
+            await raffle.enterRaffle({ value: raffleEntranceFee });
+            const winnerStartingBalance = await accounts[0].getBalance();
+
+          });
+
+
+
+        });
       });
     });
